1e71c10d3919d8d11fe0272b1e79048dc98407fa
Bob <bob.e.baxter@gmail.com>
2019-09-16 18:50:42 -0500

    Add project files.

---
+++ DataStorage/Program.cs
@@ -0,0 +1,22 @@
+﻿using DataStorage.Data;
+using System;
+
+namespace DataStorage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userRepository = new UserRepository();
+
+            Console.WriteLine("Enter the user's first name");
+            var firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter the user's last name");
+            var LastName = Console.ReadLine();
+
+            Console.WriteLine("Enter the user's password");
+            var password = Console.ReadLine();
+        }
+    }
+}
